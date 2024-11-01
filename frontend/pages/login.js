b27debe184e510ddb36bbf9f4f456ade0b00856e
Muskan Mittal <mittalmuskan758@gmail.com>
2024-11-01 17:14:15 +0530

    working on login ui

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo_image from "../public/images/Side_Image.png";
 import Image from "next/image";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
@@ -16,34 +16,70 @@ const LoginPage = () => {
             style={{ maxWidth: "100%", height: "auto" }}
           />
         </div>
-        <div className="w-full md:w-1/2">
+        <div className="w-full md:w-1/2 flex items-center justify-center">
           <Box
             component="form"
             sx={{
-              "& > :not(style)": { m: 1, width: "100%" },
-              maxWidth: "100%",
-              overflow: "hidden",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "100%",
+              maxWidth: "70%",
+              padding: 2,
+              "& > :not(style)": { mb: 2, width: "80%" },
             }}
             noValidate
             autoComplete="off"
           >
-            <TextField id="standard-basic" label="Name" variant="standard" />
+            <Typography variant="h1" component="h2" align="left">
+              Create an account
+            </Typography>
+            <Typography variant="body1" align="left" sx={{ width: "100%" }}>
+              Enter your details below
+            </Typography>
+            <TextField id="name" label="Name" variant="standard" />
             <TextField
-              id="standard-basic"
+              id="email"
               label="Email or Phone Number"
               variant="standard"
             />
             <TextField
-              id="standard-basic"
+              id="password"
               label="Password"
               variant="standard"
+              type="password"
             />
-          </Box>
-          <div className="w-full pt-3 text-center">
-            <Button variant="contained" color="error">
+
+            <Button
+              variant="contained"
+              color="error"
+              sx={{ mt: 2, width: "80%" }}
+            >
               Create Account
             </Button>
-          </div>
+            <Button
+              variant="outlined"
+              color="primary"
+              sx={{ mt: 2, width: "80%" }}
+            >
+              Sign up with Google
+            </Button>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              sx={{ width: "100%", mt: 2 }}
+            >
+              <Typography variant="body1">Already have an account?</Typography>
+              <Button
+                variant="text"
+                color="primary"
+                sx={{ ml: 1, textTransform: "none" }}
+              >
+                Log in
+              </Button>
+            </Box>
+          </Box>
         </div>
       </div>
     </section>
