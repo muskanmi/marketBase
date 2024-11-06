053819108cdecdfafba66b3efb0f800bf2d6b837
Muskan Mittal <mittalmuskan758@gmail.com>
2024-11-06 23:57:43 +0530

    working home page UI

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import banner from "../public/images/banner_image.png";
+
 export default function Home() {
   return (
-    <section>
-      <div>Home Page</div>
+    <section className="overflow-x-hidden">
+      <div className="container mx-auto px-4 md:px-8">
+        <div className="flex flex-col justify-between w-full md:flex-row mb-28">
+          <div>
+            <ul>
+              <li>Woman's Fashion</li>
+              <li>Men's Fashion</li>
+              <li>Electronics</li>
+              <li>Home & Lifestyle</li>
+              <li>Medicine</li>
+              <li>Sports & Outdoor</li>
+              <li>Baby’s & Toys</li>
+              <li>Groceries & Pets</li>
+              <li>Health & Beauty</li>
+            </ul>
+          </div>
+          <div>
+            <Image
+              src={banner}
+              alt="banner image"
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
