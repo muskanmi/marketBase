90782fd56b3cea6735323a7c655cf66c77a700b4
Muskan Mittal <mittalmuskan758@gmail.com>
2024-11-08 00:04:44 +0530

    changes made in home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,7 @@ import Banner from "@/components/Banner";
 import Card from "@/components/Card";
 import { Button } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 const theme = createTheme({
@@ -30,31 +31,26 @@ export default function Home() {
         </div>
         <div className="flex items-center justify-between">
           <h1 className="pt-5 pb-5 pr-16 text-2xl font-bold">Flash Sales</h1>
-          <div className="flex items-center space-x-3 text-2xl font-semibold">
-            <div className="flex flex-col items-center">
-              <span className="text-sm">Days</span>
-              <span className="pr-3 text-black">03</span>
-            </div>
-            <span className="text-2xl">:</span>
-
-            <div className="flex flex-col items-center">
-              <span className="text-sm">Hours</span>
-              <span className="pr-3 text-black">23</span>
-            </div>
-            <span className="text-2xl">:</span>
-
-            <div className="flex flex-col items-center">
-              <span className="text-sm">Minutes</span>
-              <span className="pr-3 text-black">19</span>
-            </div>
-            <span className="text-2xl">:</span>
-
-            <div className="flex flex-col items-center">
-              <span className="text-sm">Seconds</span>
-              <span className="text-black ">56</span>
-            </div>
+          {/* Countdown Timer */}
+          <div className="flex items-center text-2xl font-semibold space-x-4">
+            {["Days", "Hours", "Minutes", "Seconds"].map((label, index) => (
+              <div key={label} className="flex flex-col items-center">
+                <span className="text-[10px] font-light">{label}</span>
+                <div className="flex items-center">
+                  <span className="text-black">
+                    {["03", "23", "19", "56"][index]}
+                  </span>
+                  {index < 3 && (
+                    <span className="text-2xl font-semibold text-black mx-1">
+                      :
+                    </span>
+                  )}
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="flex items-center ml-auto space-x-3">
+          {/* Navigation Arrows */}
+          <div className="flex items-center space-x-3 ml-auto">
             <div className="p-2 bg-[#f5f5f5] rounded-full flex items-center justify-center">
               <FaArrowLeft size="1.2rem" />
             </div>
