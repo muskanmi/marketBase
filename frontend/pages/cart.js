5fd6a9b1fcdbbec54579d85cbb28336b6d342526
Muskan Mittal <mittalmuskan758@gmail.com>
2024-11-14 21:14:06 +0530

    working on cart UI

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <section className="container">
+      <div className="mt-9 mb-9">
+        <span>Home</span>
+        <span>/</span>
+        <span>Cart</span>
+      </div>
+
+      <div className="flex justify-between items-center">
+        <div>Product</div>
+        <div>Price</div>
+        <div>Quantity</div>
+        <div>Subtotal</div>
+      </div>
+    </section>
+  );
 };
 
 export default Cart;
